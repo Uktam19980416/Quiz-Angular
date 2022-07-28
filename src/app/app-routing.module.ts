3a8b9db988e3ee9d9b31616261fa93c61fb97a47
Uktam19980416 <rakhmatullayev2019@mail.ru>
2022-07-28 16:29:44 +0200

    2nd push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { QuestionComponent } from './question/question.component';
+import { HeaderComponent } from './header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "welcome",
+    pathMatch: "full"
+  },
+  {
+    path: "welcome",
+    component: WelcomeComponent
+  },
+  {
+    path: "question",
+    component: QuestionComponent
+  },
+  {
+    path: "header",
+    component: HeaderComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
